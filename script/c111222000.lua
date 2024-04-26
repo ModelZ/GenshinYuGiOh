f5cdc3cdee7aba1456e0e22f18b99ddb9273de06
ModelZ <model5053@gmail.com>
2024-04-26 23:09:30 +0700

    add furina effect 3

--- script/c111222000.lua
+++ script/c111222000.lua
@@ -18,39 +18,64 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate) -- effect resolve
 	c:RegisterEffect(e1)
- 
+
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.chainreg)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_COUNTER)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetCode(EVENT_CHAIN_SOLVED)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetOperation(s.acop)
+	c:RegisterEffect(e3)
+
 end
 s.listed_series={0x5003}
 Debug.Message("debug active")
+
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+	Debug.Message("s.acop active")
+	if re:IsHasCategory(CATEGORY_DRAW) or re:IsHasCategory(CATEGORY_SEARCH)e:GetHandler():GetFlagEffect(1)>0 then
+		e:GetHandler():AddCounter(0x300,1)
+	end
+
+end
+
 function s.tgfilter(c)
-	Debug.Message("s.tgfilter active")
+	--Debug.Message("s.tgfilter active")
 	return c:IsMonster() and c:IsAbleToHand()
 end
 
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	Debug.Message("s.condition active")
+	--Debug.Message("s.condition active")
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	--if chkc then Debug.Message("chkc active") end
 	if chk == 0 then 
-	Debug.Message("chk == 0 active")
+	--Debug.Message("chk == 0 active")
 	--return Duel.IsExistingMatchingCard(s.tgfilter,tp,0,LOCATION_MZONE,1,nil)
 	return true --always add at least Counter
 	end
 
-	Debug.Message("s.target active")
+	--Debug.Message("s.target active")
 	--local tg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,0,LOCATION_MZONE,1,1,nil)
 	--Debug.Message(tg)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	Debug.Message("s.activate active")
+	--Debug.Message("s.activate active")
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
 	local tg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,0,LOCATION_MZONE,1,1,nil)
-	Debug.Message(tg)
+	--Debug.Message(tg)
 	Duel.SendtoHand(tg,nil,REASON_EFFECT) -- return to hand
 	e:GetHandler():AddCounter(0x300,1) -- add 1 Lullaby Counter
 end
