e749ee7acda636974b4027eb7c891d0010deb492
ModelZ <model5053@gmail.com>
2025-09-14 05:44:40 +0700

    Sun 09/14/2025 : add faruzan effect

---
+++ script/c111222003.lua
@@ -0,0 +1,95 @@
+--Faruzan
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Special Summon 1 "Genshin" monster from Deck
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+
+    -- Fusion Summon if you control 2+ "Genshin" monsters
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1,id+100)
+    e3:SetCondition(s.fuscon)
+    e3:SetTarget(s.fustg)
+    e3:SetOperation(s.fusop)
+    c:RegisterEffect(e3)
+end
+
+s.listed_series={0x700}
+
+-- Filter for "Genshin" monsters in Deck
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(0x700) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+-- Target for Special Summon from Deck
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+
+-- Operation: Special Summon from Deck
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
+end
+
+-- Condition for Fusion Summon: control 2 or more "Genshin" monsters
+function s.fuscon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0x700)>=2
+end
+
+-- Filter for "Genshin" Fusion monsters
+function s.fusfilter(c,e,tp,m,f,chkf)
+    return c:IsType(TYPE_FUSION) and c:IsSetCard(0x700)
+        and (not f or f(c))
+        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+        and c:CheckFusionMaterial(m,nil,chkf)
+end
+
+-- Target for Fusion Summon
+function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then
+        local chkf=tp
+        local mg=Duel.GetFusionMaterial(tp)
+        return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,nil,chkf)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+
+-- Operation: Fusion Summon
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+    local chkf=tp
+    local mg=Duel.GetFusionMaterial(tp)
+    local sg=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg,nil,chkf)
+    if #sg>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local fc=sg:Select(tp,1,1,nil):GetFirst()
+        local mat=Duel.SelectFusionMaterial(tp,fc,mg,nil,chkf)
+        fc:SetMaterial(mat)
+        Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+        Duel.BreakEffect()
+        Duel.SpecialSummon(fc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+        fc:CompleteProcedure()
+    end
+end
