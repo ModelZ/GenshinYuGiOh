6e621d10821aaaaf4113f320b8e6c89f990820a1
ModelZ <model5053@gmail.com>
2025-09-14 13:39:22 +0700

    Sun 09/14/2025 : fix My Brother's card 13

--- script/c111222014.lua
+++ script/c111222014.lua
@@ -1,145 +1,88 @@
--- My Brother's
--- Scripted by [Your Name]
+--My Brother's
+--Scripted by [Your Name]
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Special Summon fusion materials when Genshin Fusion monster leaves field
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_LEAVE_FIELD)
-    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-    e1:SetCondition(s.condition)
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.activate)
-    c:RegisterEffect(e1)
+	--Special Summon fusion materials when a "Genshin" Fusion leaves by opponent
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
 end
+s.listed_series={0x700} --"Genshin"
 
-s.listed_series={0x700} -- "Genshin" archetype code
-
--- Check if a "Genshin" Fusion monster left the field by opponent's card effect
+--Check condition: your "Genshin" Fusion monster left by opponent's effect
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-    Debug.Message("Checking condition...")
-    local tc=eg:GetFirst()
-    while tc do
-        Debug.Message("Inspecting card: " .. tc:GetCode())
-        -- Must be your "Genshin" Fusion monster
-        if tc:IsControler(tp) 
-            and tc:IsSetCard(0x700) 
-            and tc:IsType(TYPE_FUSION)
-            and tc:IsPreviousLocation(LOCATION_MZONE)
-            -- Must be removed by opponent's card effect
-            and (r&REASON_EFFECT)~=0
-            and rp==1-tp then
-            Debug.Message("Condition met for card: " .. tc:GetCode())
-            -- Store the materials for later use
-            local mat=tc:GetMaterial()
-            if mat and mat:GetCount()>0 then
-                e:SetLabelObject(mat)
-                return true
-            end
-        end
-        tc=eg:GetNext()
-    end
-    Debug.Message("Condition not met.")
-    return false
+	local tc=eg:GetFirst()
+	while tc do
+		if tc:IsPreviousLocation(LOCATION_MZONE) and tc:IsType(TYPE_FUSION)
+			and tc:IsSetCard(0x700) and tc:IsControler(tp)
+			and rp==1-tp and (r&REASON_EFFECT)~=0 then
+			-- Store the materials that were actually used
+			local mat=tc:GetMaterial()
+			if #mat>0 then
+				e:SetLabelObject(mat:Clone()) -- clone to preserve group
+				return true
+			end
+		end
+		tc=eg:GetNext()
+	end
+	return false
 end
 
--- Target: Check if we can Special Summon ALL materials
+--Filter: can this specific material be summoned?
+function s.spfilter(c,e,tp,code)
+	return c:IsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+--Target check
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        local mat=e:GetLabelObject()
-        if not mat then return false end
-        
-        local mat_count=mat:GetCount()
-        local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-        if ft<mat_count then return false end -- Need exact space for all materials
-        
-        -- Check if ALL materials can be found and summoned
-        for tc in aux.Next(mat) do
-            local found=false
-            -- Check Hand
-            local hg=Duel.GetMatchingGroup(function(c) return c:GetCode()==tc:GetCode() and s.spfilter(c,e,tp) end,tp,LOCATION_HAND,0,nil)
-            if hg:GetCount()>0 then found=true end
-            
-            -- Check Graveyard
-            if not found then
-                local gg=Duel.GetMatchingGroup(function(c) return c:GetCode()==tc:GetCode() and s.spfilter(c,e,tp) end,tp,LOCATION_GRAVE,0,nil)
-                if gg:GetCount()>0 then found=true end
-            end
-            
-            -- Check Banished
-            if not found then
-                local rg=Duel.GetMatchingGroup(function(c) return c:GetCode()==tc:GetCode() and s.spfilter(c,e,tp) end,tp,LOCATION_REMOVED,0,nil)
-                if rg:GetCount()>0 then found=true end
-            end
-            
-            -- Check Deck
-            if not found then
-                local dg=Duel.GetMatchingGroup(function(c) return c:GetCode()==tc:GetCode() and s.spfilter(c,e,tp) end,tp,LOCATION_DECK,0,nil)
-                if dg:GetCount()>0 then found=true end
-            end
-            
-            -- If any material can't be found, can't activate
-            if not found then return false end
-        end
-        return true
-    end
-    
-    local mat=e:GetLabelObject()
-    local ct=mat:GetCount()
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ct,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED)
+	local mat=e:GetLabelObject()
+	if chk==0 then
+		if not mat then return false end
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<#mat then return false end
+		--Make sure every material is summonable from at least one zone
+		for mc in aux.Next(mat) do
+			if not Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp,mc:GetCode()) then
+				return false
+			end
+		end
+		return true
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,#mat,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED)
 end
 
--- Filter for materials that can be Special Summoned
-function s.spfilter(c,e,tp)
-    return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-        and (c:IsLocation(LOCATION_HAND) or c:IsLocation(LOCATION_DECK) 
-        or c:IsLocation(LOCATION_GRAVE) or c:IsLocation(LOCATION_REMOVED))
-end
-
--- Operation: Special Summon the fusion materials
+--Activate: summon those exact materials
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    Debug.Message("Activating 'My Brother's' effect...")
-    local mat=e:GetLabelObject()
-    if not mat then return end
-    
-    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-    if ft<=0 then return end
-    
-    -- Find materials that can be summoned from all zones
-    local summonable=Group.CreateGroup()
-    for tc in aux.Next(mat) do
-        Debug.Message("Checking material: " .. tc:GetCode())
-        -- Search in Hand
-        local hg=Duel.GetMatchingGroup(function(c) return c:GetCode()==tc:GetCode() and s.spfilter(c,e,tp) end,tp,LOCATION_HAND,0,nil)
-        if hg:GetCount()>0 then summonable:Merge(hg) end
-        
-        -- Search in Deck
-        local dg=Duel.GetMatchingGroup(function(c) return c:GetCode()==tc:GetCode() and s.spfilter(c,e,tp) end,tp,LOCATION_DECK,0,nil)
-        if dg:GetCount()>0 then summonable:Merge(dg) end
-        
-        -- Search in Graveyard
-        local gg=Duel.GetMatchingGroup(function(c) return c:GetCode()==tc:GetCode() and s.spfilter(c,e,tp) end,tp,LOCATION_GRAVE,0,nil)
-        if gg:GetCount()>0 then summonable:Merge(gg) end
-        
-        -- Search in Banished Zone
-        local rg=Duel.GetMatchingGroup(function(c) return c:GetCode()==tc:GetCode() and s.spfilter(c,e,tp) end,tp,LOCATION_REMOVED,0,nil)
-        if rg:GetCount()>0 then summonable:Merge(rg) end
-    end
-    Debug.Message("Total summonable materials found: " .. summonable:GetCount())
-    Debug.Message("Object" .. mat:GetMaterialCount())
-    if summonable:GetCount()>=mat.GetMaterialCount then
-        local ct=math.min(ft,mat.GetMaterialCount())
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local sg=mat:Select(tp,1,ct,nil)
-        
-        if Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)>0 then
-            -- Shuffle deck if we summoned from deck
-            local deck_summoned=sg:Filter(Card.IsLocation,nil,LOCATION_DECK)
-            if deck_summoned:GetCount()>0 then
-                Duel.ShuffleDeck(tp)
-            end
-        end
-    end
+	local mat=e:GetLabelObject()
+	if not mat then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<#mat then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+
+	local g=Group.CreateGroup()
+	for mc in aux.Next(mat) do
+		-- search for 1 copy of that exact material (priority: GY > Banished > Hand > Deck)
+		local locs={LOCATION_GRAVE,LOCATION_REMOVED,LOCATION_HAND,LOCATION_DECK}
+		local chosen=nil
+		for _,loc in ipairs(locs) do
+			local cand=Duel.GetFirstMatchingCard(s.spfilter,tp,loc,0,nil,e,tp,mc:GetCode())
+			if cand then
+				chosen=cand
+				break
+			end
+		end
+		if chosen then g:AddCard(chosen) end
+	end
+
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		if g:IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then
+			Duel.ShuffleDeck(tp)
+		end
+	end
 end
