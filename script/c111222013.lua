0a28450be2214bd518adc0897c1f0f6dd66c7cbf
ModelZ <model5053@gmail.com>
2025-09-14 09:57:11 +0700

    Sun 09/14/2025 : fix Lumine Fusion

--- script/c111222013.lua
+++ script/c111222013.lua
@@ -1,7 +1,6 @@
---Lumine Fusion
+-- Lumine Fusion
 local s,id=GetID()
 function s.initial_effect(c)
-	--Fusion Summon 1 "Genshin" Fusion Monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
@@ -13,52 +12,67 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 
--- Opponent’s monsters are treated as "Genshin" monsters (for elemental slot)
-function s.fcheck(tp,sg,fc)
-	-- must contain exactly 1 monster you control + 1 monster opponent controls
-	return sg:IsExists(Card.IsControler,1,nil,tp)
-	   and sg:IsExists(Card.IsControler,1,nil,1-tp)
-end
+-- Fusion Recipes
+s.fusion_recipes = {
+	-- FusionMonster = {your_origin, opponent_element}
+	[FusionMonsterFurinaID] = {"Fontaine","WATER"},
+	[FusionMonsterKusanaliID] = {"Sumeru","LIGHT"},
+	[FusionMonsterRaidenID] = {"Inazuma","DARK"},
+	[FusionMonsterBarbatosID] = {"Mondstadt","WIND"},
+	[FusionMonsterRexLapisID] = {"Liyue","EARTH"},
+}
 
--- Extra material pool = opponent’s face-up monsters
-function s.fextra(e,tp,mg)
-	return Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil),s.fcheck
-end
-
--- Allowed Fusion Monsters
--- Furina: Fontaine + WATER
--- Kusanali: Sumeru + LIGHT
--- Raiden Shogun: Inazuma + DARK
--- Barbatos: Mondstadt + WIND
--- Rex Lapis: Liyue + EARTH
-function s.matfilter(c)
-	return c:IsSetCard(0x700) -- all are "Genshin"
+-- Check if materials are valid
+function s.valid_materials(tp,fc)
+	local mats1=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	local mats2=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	for _,recipe in pairs(s.fusion_recipes) do
+		for mc1 in aux.Next(mats1) do
+			for mc2 in aux.Next(mats2) do
+				if mc1.origin==recipe[1] and mc2.element==recipe[2] then
+					return true
+				end
+			end
+		end
+	end
+	return false
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local chkf=tp
-		return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_EXTRA,0,1,nil,TYPE_FUSION)
-			and Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_MZONE,0,1,nil)
-	end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_EXTRA,0,1,nil,TYPE_FUSION)
+		and s.valid_materials(tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp)
-	-- add opponent’s face-up monsters as possible extra materials
-	local mg2=s.fextra(e,tp,mg1)
-	mg1:Merge(mg2)
-	local sg=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_EXTRA,0,nil,TYPE_FUSION)
-	if #sg==0 then return end
+	local fusion_candidates=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_EXTRA,0,nil,TYPE_FUSION)
+	local mats1=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	local mats2=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	local valid_fc=nil
+
+	-- find a fusion monster that can be summoned
+	for fc in aux.Next(fusion_candidates) do
+		local recipe=s.fusion_recipes[fc:GetCode()]
+		if recipe then
+			for mc1 in aux.Next(mats1) do
+				for mc2 in aux.Next(mats2) do
+					if mc1.origin==recipe[1] and mc2.element==recipe[2] then
+						valid_fc=fc
+						goto found
+					end
+				end
+			end
+		end
+	end
+	::found::
+	if not valid_fc then return end
+
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=sg:Select(tp,1,1,nil):GetFirst()
-	if not tc then return end
-	local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,tp)
-	if not mat1 then return end
-	tc:SetMaterial(mat1)
-	Duel.SendtoGrave(mat1,REASON_MATERIAL+REASON_FUSION)
-	Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-	tc:CompleteProcedure()
+	local mat1=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	local mat2=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+
+	valid_fc:SetMaterial(Group.FromCards(mat1,mat2))
+	Duel.SendtoGrave(Group.FromCards(mat1,mat2),REASON_MATERIAL+REASON_FUSION)
+	Duel.SpecialSummon(valid_fc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+	valid_fc:CompleteProcedure()
 end
