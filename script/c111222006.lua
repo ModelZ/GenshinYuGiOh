bd68ff08afcb6287dae9caa547209665e1ca2925
ModelZ <model5053@gmail.com>
2025-09-14 17:48:45 +0700

    Sun 09/14/2025 : fix babartos negate 2

--- script/c111222006.lua
+++ script/c111222006.lua
@@ -111,7 +111,7 @@ function s.negcon(e,tp,eg,ep,ev,re,r,rp)
     local prev = Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT)
     if not prev then return false end
     local rc = prev:GetHandler()
-    if not rc:IsSetCard(0x700) then return false end -- 0x700 = "Genshin" archetype
+    if not rc:IsSetCard(0x700) and not rc==1-tp then return false end -- 0x700 = "Genshin" archetype
 
     -- Make sure the opponent's monster is still related to its effect
     return re:GetHandler():IsRelateToEffect(re)
