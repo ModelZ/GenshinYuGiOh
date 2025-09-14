8b4dd3c8b8f4657ad9d357178605d435b5c4e7a3
ModelZ <model5053@gmail.com>
2025-09-14 10:55:37 +0700

    Sun 09/14/2025 : fix Lumine Fusion 7

--- script/c111222013.lua
+++ script/c111222013.lua
@@ -14,7 +14,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 
-s.listed_series={0x700} --Replace with actual "Genshin" archetype code
+s.listed_series={0x700} --"Genshin" archetype code
 
 --Filter for "Genshin" monsters on your field
 function s.filter1(c,e)
@@ -29,8 +29,7 @@ end
 --Filter for "Genshin" Fusion monsters in Extra Deck
 function s.spfilter(c,e,tp,m,f,chkf)
 	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x700) and (not f or f(c))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) 
-		and c:CheckFusionMaterial(m,nil,chkf)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
 end
 
 --Check specific fusion requirements for each boss monster
@@ -41,37 +40,33 @@ function s.fusioncheck(fc,sg,tp)
 	if g1:GetCount()~=1 or g2:GetCount()~=1 then return false end
 	if not g1:IsExists(Card.IsSetCard,1,nil,0x700) then return false end
 	
-	local tc=fc
-	local code=tc:GetCode()
+	local code=fc:GetCode()
+	local mymon=g1:GetFirst()
+	local oppmon=g2:GetFirst()
 	
-	--Furina De Fontaine: 1 "Fontaine" + 1 "Genshin" WATER
-	if code==12345001 then --Replace with actual card code
-		return (g1:IsExists(Card.IsSetCard,1,nil,0x5700) or g2:IsExists(Card.IsSetCard,1,nil,0x5700)) --Fontaine archetype
-			and (g1:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_WATER) or g2:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_WATER))
+	--Furina De Fontaine: My "Fontaine" monster + opponent monster with WATER attribute
+	if code==12345001 then
+		return mymon:IsSetCard(0x5700) and oppmon:IsAttribute(ATTRIBUTE_WATER)
 	end
 	
-	--Lesser Lord Kusanali: 1 "Sumeru" + 1 "Genshin" LIGHT  
-	if code==111222007 then --Replace with actual card code
-		return (g1:IsExists(Card.IsSetCard,1,nil,0x4700) or g2:IsExists(Card.IsSetCard,1,nil,0x4700)) --Sumeru archetype
-			and (g1:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_LIGHT) or g2:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_LIGHT))
+	--Lesser Lord Kusanali: My "Sumeru" monster + opponent monster with LIGHT attribute  
+	if code==111222007 then
+		return mymon:IsSetCard(0x4700) and oppmon:IsAttribute(ATTRIBUTE_LIGHT)
 	end
 	
-	--Raiden Shogun: 1 "Inazuma" + 1 "Genshin" DARK
-	if code==111222009 then --Replace with actual card code
-		return (g1:IsExists(Card.IsSetCard,1,nil,0x3700) or g2:IsExists(Card.IsSetCard,1,nil,0x3700)) --Inazuma archetype
-			and (g1:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_DARK) or g2:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_DARK))
+	--Raiden Shogun: My "Inazuma" monster + opponent monster with DARK attribute
+	if code==111222009 then
+		return mymon:IsSetCard(0x3700) and oppmon:IsAttribute(ATTRIBUTE_DARK)
 	end
 	
-	--Barbatos: 1 "Mondstadt" + 1 "Genshin" WIND
-	if code==111222006 then --Replace with actual card code
-		return (g1:IsExists(Card.IsSetCard,1,nil,0x1700) or g2:IsExists(Card.IsSetCard,1,nil,0x1700)) --Mondstadt archetype
-			and (g1:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_WIND) or g2:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_WIND))
+	--Barbatos: My "Mondstadt" monster + opponent monster with WIND attribute
+	if code==111222006 then
+		return mymon:IsSetCard(0x1700) and oppmon:IsAttribute(ATTRIBUTE_WIND)
 	end
 	
-	--Rex Lapis: 1 "Liyue" + 1 "Genshin" EARTH
-	if code==111222008 then --Replace with actual card code
-		return (g1:IsExists(Card.IsSetCard,1,nil,0x2700) or g2:IsExists(Card.IsSetCard,1,nil,0x2700)) --Liyue archetype
-			and (g1:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_EARTH) or g2:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_EARTH))
+	--Rex Lapis: My "Liyue" monster + opponent monster with EARTH attribute
+	if code==111222008 then
+		return mymon:IsSetCard(0x2700) and oppmon:IsAttribute(ATTRIBUTE_EARTH)
 	end
 	
 	--Default check for any "Genshin" Fusion
@@ -80,30 +75,23 @@ end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local chkf=tp
 		local ownmg=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_MZONE,0,nil,e)
 		local oppmg=Duel.GetMatchingGroup(s.filter2,tp,0,LOCATION_MZONE,nil,e)
 		if #ownmg==0 or #oppmg==0 then return false end
 		
-		--Create combined material group
-		local mg=Group.CreateGroup()
-		mg:Merge(ownmg)
-		mg:Merge(oppmg)
-		
-		--Check if any valid fusion target exists
-		local exg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg,nil,chkf)
+		--Check if any valid fusion exists
+		local exg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp,nil,nil,tp)
 		for tc in aux.Next(exg) do
-			local fmat=mg:Filter(Card.CheckFusionMaterial,nil,tc,nil,chkf)
-			if fmat:GetCount()>=2 then
-				local res=false
-				for i=1,fmat:GetCount()-1 do
-					local sg=fmat:Select(tp,2,2,nil,0)
-					if sg and s.fusioncheck(tc,sg,tp) then
-						res=true
-						break
+			--Check all combinations of my monsters + opponent monsters
+			for mymon in aux.Next(ownmg) do
+				for oppmon in aux.Next(oppmg) do
+					local sg=Group.CreateGroup()
+					sg:AddCard(mymon)
+					sg:AddCard(oppmon)
+					if s.fusioncheck(tc,sg,tp) then
+						return true
 					end
 				end
-				if res then return true end
 			end
 		end
 		return false
@@ -112,52 +100,49 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local chkf=tp
 	local ownmg=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_MZONE,0,nil,e)
 	local oppmg=Duel.GetMatchingGroup(s.filter2,tp,0,LOCATION_MZONE,nil,e)
 	if #ownmg==0 or #oppmg==0 then return end
 	
-	local mg=Group.CreateGroup()
-	mg:Merge(ownmg)
-	mg:Merge(oppmg)
-	
-	local exg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg,nil,chkf)
+	local exg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp,nil,nil,tp)
 	if #exg==0 then return end
 	
+	--Select fusion target
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local tc=exg:Select(tp,1,1,nil):GetFirst()
 	
-	--Select materials with specific requirements
-	local fmat=mg:Filter(Card.CheckFusionMaterial,nil,tc,nil,chkf)
-	local validgroups={}
-	
-	--Find all valid 2-card combinations
-	for i=1,fmat:GetCount()-1 do
-		for j=i+1,fmat:GetCount() do
+	--Find valid material combinations for selected fusion monster
+	local validcombos={}
+	for mymon in aux.Next(ownmg) do
+		for oppmon in aux.Next(oppmg) do
 			local sg=Group.CreateGroup()
-			sg:AddCard(fmat:GetCardByIndex(i-1))
-			sg:AddCard(fmat:GetCardByIndex(j-1))
+			sg:AddCard(mymon)
+			sg:AddCard(oppmon)
 			if s.fusioncheck(tc,sg,tp) then
-				table.insert(validgroups,sg)
+				table.insert(validcombos,sg)
 			end
 		end
 	end
 	
-	if #validgroups==0 then return end
+	if #validcombos==0 then return end
 	
-	--Let player choose from valid combinations
+	--Select materials
 	local selected_group
-	if #validgroups==1 then
-		selected_group=validgroups[1]
+	if #validcombos==1 then
+		selected_group=validcombos[1]
 	else
-		--Manual selection if multiple valid combinations
+		--Let player choose materials manually
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
 		local g1=ownmg:Select(tp,1,1,nil)
 		local g2=oppmg:Select(tp,1,1,nil)
 		selected_group=Group.CreateGroup()
 		selected_group:Merge(g1)
 		selected_group:Merge(g2)
-		if not s.fusioncheck(tc,selected_group,tp) then return end
+		
+		--Verify selection is valid
+		if not s.fusioncheck(tc,selected_group,tp) then 
+			return 
+		end
 	end
 	
 	--Perform fusion summon
