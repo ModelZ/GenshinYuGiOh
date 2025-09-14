1ee34afae3c1f023050eb1e247c6a77f7b25f69d
ModelZ <model5053@gmail.com>
2025-09-14 09:01:50 +0700

    Sun 09/14/2025 : TCG Genshin Card Game added

--- script/c111222012.lua
+++ script/c111222012.lua
@@ -1,4 +1,33 @@
---Barbatos
-function c111222006.initial_effect(c)
-	
+--TCG Genshin Card Game
+local s,id=GetID()
+function s.initial_effect(c)
+	--Add 1 "Genshin" card from your Deck to your hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x700}
+
+--Filter for any "Genshin" card
+function s.filter(c)
+	return c:IsSetCard(0x700) and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
