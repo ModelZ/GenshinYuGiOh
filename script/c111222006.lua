5d0bd5117cb5a75fa7f6518b6534094b568fbc1a
ModelZ <model5053@gmail.com>
2025-09-14 15:43:19 +0700

    Sun 09/14/2025 : Ad Astra Abyssosque! Added

--- script/c111222006.lua
+++ script/c111222006.lua
@@ -1,4 +1,64 @@
---Barbatos
-function c111222006.initial_effect(c)
-	
+--Ad Astra Abyssosque!
+--Scripted by ChatGPT (custom card)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--cannot respond
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_INACTIVATE)
+	e2:SetValue(function(e,ct) return true end)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_CANNOT_DISEFFECT)
+	c:RegisterEffect(e3)
+end
+
+--Check if you control a "Genshin" Fusion Monster
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xgenshin) and c:IsType(TYPE_FUSION)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsChainNegatable(ev) and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+
+--Tribute cost: 1 "Genshin" Fusion Monster
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,s.cfilter,1,nil) end
+	local g=Duel.SelectReleaseGroup(tp,s.cfilter,1,1,nil)
+	Duel.Release(g,REASON_COST)
+end
+
+--Negate + destroy + lock card name
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		if Duel.Destroy(eg,REASON_EFFECT)>0 then
+			--Lock name for the rest of the Duel
+			local c=e:GetHandler()
+			local code=re:GetHandler():GetOriginalCode()
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+			e1:SetTargetRange(0,1)
+			e1:SetValue(function(e,re,tp) return re:GetHandler():IsCode(code) end)
+			Duel.RegisterEffect(e1,tp,true)
+		end
+	end
 end
