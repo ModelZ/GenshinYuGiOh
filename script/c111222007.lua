ec82ac159e5b1e205356aa1f90d8587a8ca7a0f9
ModelZ <model5053@gmail.com>
2025-09-14 21:32:58 +0700

    Sun 09/14/2025 : debug

--- script/c111222007.lua
+++ script/c111222007.lua
@@ -14,12 +14,13 @@ function s.initial_effect(c)
     e1:SetOperation(s.chainop)
     c:RegisterEffect(e1)
 
-	-- Continuous effect: observe counters
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    e3:SetCode(EVENT_ADJUST) -- triggers every game state update
+	-- Add Akara Counters
+	local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+    e3:SetCode(EVENT_ADD_COUNTER) -- triggers whenever counters are placed
     e3:SetRange(LOCATION_MZONE)
-    e3:SetOperation(s.observe_counter)
+    e3:SetOperation(s.acop)
     c:RegisterEffect(e3)
 
 	-- Quick effect: prevent destruction or damage
@@ -35,26 +36,6 @@ function s.initial_effect(c)
     -- You can remove any number of Akara counter(s) and target your monster that had the counter on it on the field (Quick Effect); 
     -- increase the number of that Counter by the number of removed Akara counter(s)..
 
-end
-
--- Table to store last known counter counts
-s.last_counters = {}
-
--- Observe counters on all face-up cards and add Akara Counter to this card if another card gains counters
-function s.observe_counter(e,tp,eg,ep,ev,re,r,rp)
-    local c = e:GetHandler()
-    local g = Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-    for tc in aux.Next(g) do
-        local id = tc:GetFieldID()
-        local last = s.last_counters[id] or 0
-        local now = tc:GetCounter(0x301)
-        if tc~=c and now>last then
-            -- Another card gained 1 or more Akara counters → give this card 1 Akara Counter
-            Debug.Message("Another card gained Akara counter")
-            e:GetHandler():AddCounter(0x301,1)
-        end
-        s.last_counters[id] = now
-    end
 end
 
 function s.IsExactSet(c,setcode)
@@ -74,26 +55,18 @@ function s.lightfilter(c,fc,sumtype,tp)
 	return c:IsSetCard(0x700) and c:IsAttribute(ATTRIBUTE_LIGHT)  -- "Genshin" LIGHT monster
 end
 
-
-function s.chainop(e,tp,eg,ep,ev,re,r,rp)
-    local rc=re:GetHandler()
-    if rc:IsSetCard(0x700) and rp==tp then
-        -- make this chain unrespondable
-        Duel.SetChainLimit(aux.FALSE)
+-- Place a Akara Counter when other's card place a counter (ignores itself)
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    -- eg = group of cards that got counters
+    for tc in aux.Next(eg) do
+        if tc~=c then
+            -- Another card got counters → give this card 1 Akara Counter
+            c:AddCounter(0x301,1)
+            break -- only once per event, no matter how many got counters
+        end
     end
 end
--- -- Place a Akara Counter when other's card place a counter (ignores itself)
--- function s.acop(e,tp,eg,ep,ev,re,r,rp)
---     local c=e:GetHandler()
---     -- eg = group of cards that got counters
---     for tc in aux.Next(eg) do
---         if tc~=c then
---             -- Another card got counters → give this card 1 Akara Counter
---             c:AddCounter(0x301,1)
---             break -- only once per event, no matter how many got counters
---         end
---     end
--- end
 
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	return true -- you can add battle/effect destruction check
