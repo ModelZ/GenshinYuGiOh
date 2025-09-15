7949da7416947a07f83b266b713dcb4804c523b1
ModelZ <model5053@gmail.com>
2025-09-16 00:51:15 +0700

    Tue 09/16/2025 : fix indestrcution effect 6

--- script/c111222007.lua
+++ script/c111222007.lua
@@ -83,7 +83,7 @@ function s.damrepop(e,tp,eg,ep,ev,re,r,rp)
     if c:GetCounter(0x301)>0 then
         c:RemoveCounter(tp,0x301,1,REASON_COST)
         local tc=Duel.GetAttackTarget()
-        if tc and tc:IsSetCard(0x700) and tc:IsControler(tp) and tc==e:GetHandler() then
+        if tc and tc:IsSetCard(0x700) and tc:IsControler(tp) then
             -- Make the target indestructible by battle and card effect this time
             local e1=Effect.CreateEffect(c)
             e1:SetType(EFFECT_TYPE_SINGLE)
