323c1d06b154c156c8b8404a682368ec05f74eb3
ModelZ <model5053@gmail.com>
2025-09-16 20:02:50 +0700

    Tue 09/16/2025 : add transfer counter nahida 10

--- script/c111222007.lua
+++ script/c111222007.lua
@@ -189,7 +189,7 @@ end
 
 -- Can target your face-up monster that can accept counters
 function s.rdcntfilter(c)
-    Debug.Message("rdcntfilter called on: faceup="..tostring(c:IsFaceup()).." hascounters="..tostring(not c:GetAllCounters()==nil))
+    Debug.Message("rdcntfilter called on: faceup="..tostring(c:IsFaceup()).." hascounters="..tostring(c:GetAllCounters()))
     return c:IsFaceup() and not c:GetAllCounters()==nil
 end
 
